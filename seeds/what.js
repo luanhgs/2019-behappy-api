b04532d119bc07eb872fc26a0fa14886ad720264
Luan Henrique <luanhgs0111@gmail.com>
2019-08-15 09:42:36 -0300

    3 feito

--- seeds/what.js
+++ seeds/what.js
@@ -1,18 +1,18 @@
-const table_name = "what";
+const table_name = "who";
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex(table_name).del()
-    .then(function () {
+  return knex(table_name)
+    .del()
+    .then(function() {
       // Inserts seed entries
       return knex(table_name).insert([
-        {title: 'Zap', image_index: 0},
-        {title: 'Bom dia', image_index: 1},
-        {title: 'Ligação', image_index: 2},
-        {title: 'Aperto de mão', image_index: 3},
-        {title: 'Abraço', image_index: 4},
-        {title: 'Doce', image_index: 5},
-        {title: 'Presente', image_index: 6}
+        { title: "Pais", image_index: 0 },
+        { title: "Filhos", image_index: 1 },
+        { title: "Cônjuges", image_index: 2 },
+        { title: "Amigos", image_index: 3 },
+        { title: "Pets", image_index: 4 },
+        { title: "Outros", image_index: 5 }
       ]);
     });
 };
